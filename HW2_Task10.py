09be5668bd7a339aa0b41f82f41373af1e60a1c6
Yuriy Shevela <g69dwine@gmail.com>
2023-06-20 23:32:15 +0300

    Task10 changed code

--- HW2_Task10.py
+++ HW2_Task10.py
@@ -2,13 +2,12 @@
 # которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное количество монет, 
 # которые нужно перевернуть
 
-import random
-coinSides = ["O","P"]
+from random import randint
 num = int(input("Сколько монет бросаем на стол? "))
 coins = []
 for i in range(num):
-    coins.append(random.choice(coinSides))
+    coins.append(randint(0,1))
     print(coins[i], end=" ")
-if coins.count("O") >= coins.count("P"):
-    print(coins.count("P"))
-else: print(coins.count("O"))
+if coins.count(1) >= coins.count(0):
+    print(f" -> {coins.count(0)}")
+else: print(f" -> {coins.count(1)}")
