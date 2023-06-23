0beec0f7869c65960ea373b2bfee27da397ca247
Yuriy Shevela <g69dwine@gmail.com>
2023-06-23 10:01:02 +0300

    Added Task18

---
+++ HW3_Task18.py
@@ -0,0 +1,24 @@
+'''
+Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X
+Пример:
+5
+1 2 3 4 5
+6
+-> 5
+'''
+
+from random import randint
+
+num = int(input("Кол-во элементов массива: "))
+array = [randint(1,100) for i in range(num)]
+print(array)
+numX = int(input("Искомое число: "))
+nearNum = array[0]
+
+for i in range(1, len(array)):
+    if abs(numX - array[i]) < abs(num - nearNum):
+        nearNum = array[i]
+        
+print(f"{nearNum} - ближайшее к {numX}")
