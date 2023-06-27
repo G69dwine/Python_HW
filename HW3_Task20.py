84e16f104ddfbff64412e9aae32ecf19ad0a31ed
Yuriy Shevela <g69dwine@gmail.com>
2023-06-27 10:03:51 +0300

    Task20 Edited

--- HW3_Task20.py
+++ HW3_Task20.py
@@ -29,10 +29,9 @@
 #     for key in abc:
 #         for i in abc[key]:
 #             finalABC[i] = key
-#     sorted(finalABC.items())
 #     return dict(sorted(finalABC.items()))
-# print(FillABC(rABC))
-# print(FillABC(eABC))
+# rusABC = FillABC(rABC)
+# engABC = FillABC(eABC)
 
 rusABC = {'А': 1, 'Б': 3, 'В': 1, 'Г': 3, 'Д': 2, 'Е': 1, 'Ё': 3, 'Ж': 5, 'З': 5, 'И': 1, 'Й': 4, 'К': 2, 'Л': 2, 'М': 2, 'Н': 1, 'О': 1, 'П': 2, 
            'Р': 1, 'С': 1, 'Т': 1, 'У': 2, 'Ф': 10, 'Х': 5, 'Ц': 5, 'Ч': 5, 'Ш': 8, 'Щ': 10, 'Ъ': 10, 'Ы': 4, 'Ь': 3, 'Э': 8, 'Ю': 8, 'Я': 3}
